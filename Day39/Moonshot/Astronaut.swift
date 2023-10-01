c40a50048bb5001eadb86edda8d89baf934533d4
Jeff <ambitious4728@gmail.com>
2023-10-01 13:46:38 +0800

    Complete Day40

---
+++ Day39/Moonshot/Astronaut.swift
@@ -0,0 +1,13 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by 褚宣德 on 2023/10/1.
+//
+
+import Foundation
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
