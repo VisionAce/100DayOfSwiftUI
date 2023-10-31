50b35474c739c9c28db0f08f9114ad33a75c8f29
Jeff <ambitious4728@gmail.com>
2023-10-31 17:01:49 +0800

    Complete Day63

---
+++ Day63/WrappingAUIViewControllerInASwiftUIView/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by 褚宣德 on 2023/10/30.
+//
+
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import SwiftUI
+
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    
+    var body: some View {
+        VStack {
+           image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
